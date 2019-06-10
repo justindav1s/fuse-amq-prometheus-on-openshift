d4996fa4f86152f3ae9894c007cb089f18c65673
Justin Davis <justinndavis@gmail.com>
2019-06-10 16:18:50 +0100

    updates

--- fuse-console/README.md
+++ fuse-console/README.md
@@ -2,5 +2,11 @@
 
 https://access.redhat.com/documentation/en-us/red_hat_fuse/7.3/html/managing_fuse/index
 
+## At github
+   * https://github.com/jboss-fuse/application-templates
+
+## Get the latest image
+   * oc import-image fuse7/fuse7-console:1.3 --from=registry.redhat.io/fuse7/fuse-console --confirm -n openshift
+
 
 Run the deploy.sh script as a cluster-admin
--- fuse-console/deploy.sh
+++ fuse-console/deploy.sh
@@ -12,6 +12,8 @@ done
 
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:${PROJECT}:template-instance-controller
 
+oc delete OAuthClient fuse73-console-oauth-client
+
 oc new-app \
     -n ${PROJECT} \
     -f https://raw.githubusercontent.com/jboss-fuse/application-templates/application-templates-2.1.fuse-730065-redhat-00002/fis-console-cluster-template.json \
