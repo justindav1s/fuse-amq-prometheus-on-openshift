a7d04d7908e7b0413f44dbf12c038a3781771b1b
Justin Davis <justinndavis@gmail.com>
2019-06-10 17:03:25 +0100

    updates

--- bin/fuse-console/deploy.sh
+++ bin/fuse-console/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT=fuse-console
+PROJECT=fuse-infra
 
 oc delete project $PROJECT
 oc new-project $PROJECT 2> /dev/null
@@ -17,4 +17,4 @@ oc delete OAuthClient fuse73-console-oauth-client
 oc new-app \
     -n ${PROJECT} \
     -f https://raw.githubusercontent.com/jboss-fuse/application-templates/application-templates-2.1.fuse-730065-redhat-00002/fis-console-cluster-template.json \
-    -p ROUTE_HOSTNAME=home-${PROJECT}.app.ocp.datr.eu
+    -p ROUTE_HOSTNAME=console-${PROJECT}.app.ocp.datr.eu
