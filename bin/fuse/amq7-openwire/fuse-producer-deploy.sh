f358e770601ad11e974a9dc5202dc98459d0ef47
Justin Davis <justinndavis@gmail.com>
2019-06-18 11:47:55 +0100

    amq7 stuff

--- bin/fuse/amq7-openwire/fuse-producer-deploy.sh
+++ bin/fuse/amq7-openwire/fuse-producer-deploy.sh
@@ -9,10 +9,9 @@ AMQ_NAMESPACE=amq7
 
 oc delete dc,svc -l app=${APP_NAME}
 
-oc process -f ../../templates/fuse-app-deployment-template.yaml \
+oc process -f ../../../templates/fuse-amq7-app-deployment-template.yaml \
   -p APP_NAME=${APP_NAME} \
-  -p AMQ_HOST="custom-amq6-broker-1-amq-amqp.${AMQ_NAMESPACE}.svc,custom-amq6-broker-2-amq-amqp.${AMQ_NAMESPACE}.svc" \
-  -p AMQ_CONNECTION_STRING="failover:(amqp://${AMQ_APP_NAME_PREFIX}-1-amq-amqp.${AMQ_NAMESPACE}.svc:5672,amqp://${AMQ_APP_NAME_PREFIX}-2-amq-amqp.${AMQ_NAMESPACE}.svc:5672)?initialReconnectDelay=100" \
+  -p AMQ_CONNECTION_STRING="tcp://broker-amq-headless.amq7.svc:61616?protocols=OPENWIRE" \
   -p AMQ_USERNAME="admin" \
   -p AMQ_PASSWORD="changeme" \
   -p APP_IMAGE=${APP_NAME} \
