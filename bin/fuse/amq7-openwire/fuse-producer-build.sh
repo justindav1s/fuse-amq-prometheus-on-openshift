d02cf99e8fde5b98ffc85c164aa5e87419a07a8d
Justin Davis <justinndavis@gmail.com>
2019-06-18 13:29:04 +0100

    amq7 stuff

--- bin/fuse/amq7-openwire/fuse-producer-build.sh
+++ bin/fuse/amq7-openwire/fuse-producer-build.sh
@@ -8,19 +8,19 @@ APP_NAME=producer-app
 BUILD_NAME=${APP_NAME}-build
 REPO_CONTEXT=fuse/amq7-openwire/${APP_NAME}
 
-oc delete is ${APP_NAME}
-oc delete bc ${BUILD_NAME}
-
-oc process -f ../../../templates/fuse-amq7-app-bc-template.yaml \
-  -p BUILD_NAME=${BUILD_NAME}  \
-  -p APPLICATION_NAME=${APP_NAME}  \
-  -p GIT_REPO="https://github.com/justindav1s/amq.git"  \
-  -p GIT_BRANCH=master  \
-  -p GIT_REPO_CONTEXT="${REPO_CONTEXT}"  \
-  -p FUSE_IMAGE="fuse7-java-openshift" \
-  -p FUSE_IMAGE_TAG="1.2" \
-  -p FUSE_IMAGE_NS="openshift" \
-  -p MAVEN_ARGS="package -DskipTests -Dfabric8.skip -e -B" | oc create -f -
+#oc delete is ${APP_NAME}
+#oc delete bc ${BUILD_NAME}
+#
+#oc process -f ../../../templates/fuse-amq7-app-bc-template.yaml \
+#  -p BUILD_NAME=${BUILD_NAME}  \
+#  -p APPLICATION_NAME=${APP_NAME}  \
+#  -p GIT_REPO="https://github.com/justindav1s/amq.git"  \
+#  -p GIT_BRANCH=master  \
+#  -p GIT_REPO_CONTEXT="${REPO_CONTEXT}"  \
+#  -p FUSE_IMAGE="fuse7-java-openshift" \
+#  -p FUSE_IMAGE_TAG="1.2" \
+#  -p FUSE_IMAGE_NS="openshift" \
+#  -p MAVEN_ARGS="package -DskipTests -Dfabric8.skip -e -B" | oc create -f -
 
 oc start-build ${BUILD_NAME}
 
