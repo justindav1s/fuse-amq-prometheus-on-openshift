d82a407a4448261b5203818122d72bcca55c5eb4
Justin Davis <justinndavis@gmail.com>
2019-06-18 13:20:41 +0100

    amq7 stuff

--- fuse/amq7-openwire/producer-app/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
+++ fuse/amq7-openwire/producer-app/src/main/java/sample/camel/SampleAutowiredAmqRoute.java
@@ -14,22 +14,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
+//<route id="generate-order-route" streamCache="true">
+//        <from id="route-timer" uri="timer:order?period=3000"/>
+//        <bean id="route-new-order" method="generateOrder" ref="orderGenerator"/>
+//        <setHeader headerName="Exchange.FILE_NAME" id="route-set-order-header">
+//        <!-- defining the header containing a simulated file name -->
+//        <method method="generateFileName" ref="orderGenerator"/>
+//        </setHeader>
+//        <log id="route-log-order" message="Generating order ${file:name}"/>
+//        <to id="route-to-incoming-orders" uri="amqp:queue:incomingOrders"/>
+//        </route>
+
 package sample.camel;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
-@Component
-public class SampleAutowiredAmqRoute extends RouteBuilder {
-
-    @Override
-    public void configure() throws Exception {
-        from("jms:foo")
-            .to("log:sample");
-
-        from("timer:bar")
-            .setBody(constant("Hello from Camel"))
-            .to("jms:foo");
-    }
-
-}
+//@Component
+//public class SampleAutowiredAmqRoute extends RouteBuilder {
+//
+//    @Override
+//    public void configure() throws Exception {
+//        from("jms:foo")
+//            .to("log:sample");
+//
+//        from("timer:bar")
+//            .setBody(constant("Hello from Camel"))
+//            .to("jms:foo");
+//    }
+//
+//}
